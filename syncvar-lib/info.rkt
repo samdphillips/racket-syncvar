adc1002e7783bcf8ca205fc080f4b52e18617dfd
Sam Phillips <samdphillips@gmail.com>
2022-12-28 17:06:33 -0800

    Reorganize into doc and lib packages

---
+++ syncvar-lib/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define name "syncvar-lib")
+(define collection "syncvar")
+(define version "0.0.1")
+(define deps '("base"))
+(define build-deps '("rackunit-lib"))
+(define pkg-authors '(samdphillips@gmail.com))
